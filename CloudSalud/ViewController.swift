9579aeb8e1dd66436b8f2f06b97362fc42b73db8
Ramses Miramontes Meza <ramses7083@MacBook-Pro-de-Ramses.local>
2015-03-17 12:16:38 -0600

    Initial Commit

---
+++ CloudSalud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudSalud
+//
+//  Created by Ramses Miramontes Meza on 17/03/15.
+//  Copyright (c) 2015 rmm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
